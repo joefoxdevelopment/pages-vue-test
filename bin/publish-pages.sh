9d688cd2354ea14ed087cc266758d82570bebdee
Joe Fox <joe@invitial.com>
2025-05-21 23:22:02 +0100

    Correct base path in vite config

--- bin/publish-pages.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-npm run lint
-npm run build
-git subtree push --prefix dist origin gh-pages
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  basename: '/pages-vue-test/',
+  base: '/pages-vue-test/',
   plugins: [
     vue(),
     vueDevTools(),
